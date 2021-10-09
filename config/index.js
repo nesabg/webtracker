0c8a7c339cca8927ca28dd64248178474f472bd4
Asen Asenov <53828105+nesabg@users.noreply.github.com>
2021-10-09 20:14:25 +0300

    Feat adding cors package

--- config/index.js
+++ config/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 
 const apiRouter = require('../routes/apiRouter')
 
 module.exports = (app) => {
 
     app.use(express.json())
+    app.use(cors())
     app.use('/api', apiRouter)
-    
+
 }
