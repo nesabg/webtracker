aac837c92ccb538f0ca6d4a60451223fc98dda5b
Asen Asenov <53828105+nesabg@users.noreply.github.com>
2021-10-16 19:31:24 +0300

    Feat adding autorization middleware

---
+++ controllers/userController.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken")
+
+const authorization = async (req, res, next) => {
+    if (!req.headers.authorization) {
+        return res.status(403).json("You are not authorized to see this page")
+    }
+
+    const token = req.headers.authorization.split(" ")[1]
+
+    try{
+
+        const status = await jwt.verify(token, process.env.JWT_SECRET)
+    
+    }catch(e){
+        return res.status(403).json("You are not authorized to see this page")
+    }
+
+  next()
+}
+
+
+module.exports = {
+  authorization,
+}
